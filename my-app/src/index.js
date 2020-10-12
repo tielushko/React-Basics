79d2828e7cb8f8d014296032a08be4974e549a3d
Oleg Tielushko <tielushko@mail.usf.edu>
2020-10-11 22:36:51 -0400

    Creating my first React component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"; //ES6 JavaScript to add react functionality as a module
+import ReactDom from "react-dom"; //importing dom dependency to use Render method to inject js into html file unnder root
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//must capitalize the name of the function
+function Greeting() {
+  //this function must return html (which is in fact officially jsx)
+  return <h4>This is Oleg and this is my first component</h4>; //html that will be displayed on the screen
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//must pass in with closing tag <Greeting/> or <Greeting></Greeting>
+ReactDom.render(<Greeting />, document.getElementById("root")); //notice we pass in the component and the div to which we are injecting it
